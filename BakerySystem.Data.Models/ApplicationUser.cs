62f0bf0a3ae67fadac5436ca15d3edffa56fb244
Denis Georgiev <128997731+Laracrofttx@users.noreply.github.com>
2023-08-01 00:01:44 +0300

    Update ApplicationUser.cs

--- BakerySystem.Data.Models/ApplicationUser.cs
+++ BakerySystem.Data.Models/ApplicationUser.cs
@@ -1,12 +1,11 @@
+namespace BakerySystem.Data.Models
+{
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace BakerySystem.Data.Models
-{
-	internal class ApplicationUser
+	public class ApplicationUser
 	{
 	}
 }
