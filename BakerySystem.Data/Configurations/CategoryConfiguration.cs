a77272c9257f0bdfc648daf8f82f4d9d23aa9e7b
Denis Georgiev <d3nata123@gmail.com>
2023-08-02 23:29:16 +0300

    Seeding data

---
+++ BakerySystem.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,54 @@
+﻿namespace BakerySystem.Data.Configurations
+{
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+	using BakerySystem.Data.Models;
+	public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+	{
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+			builder.HasData(this.GenerateCategories());
+		}
+
+
+		private Category[] GenerateCategories()
+		{
+			ICollection<Category> categories = new HashSet<Category>();
+
+			Category category;
+
+			category = new Category()
+			{
+				Id = 1,
+				Name = "Bread",
+				Description = "The most common type of bread in many countries."
+				
+			};
+			categories.Add(category);
+
+			category = new Category()
+			{
+				Id = 2,
+				Name = "Muffins",
+				Description = "An individually portioned baked product."
+
+			};
+			categories.Add(category);
+
+			category = new Category()
+			{
+				Id = 3,
+				Name = "Cakes",
+				Description = "The distinction between these two broad categories of cake is in the fat content. Foam cakes have little to no fat, and usually have a larger proportion of egg."
+
+
+
+			};
+			categories.Add(category);
+
+			return categories.ToArray();
+
+		}
+	}
+}
---
+++ BakerySystem.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using BakerySystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BakerySystem.Data.Configurations
+{
+	public class ProductConfiguration : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			builder.HasData(this.GenerateProducts());
+		}
+
+		private Product[] GenerateProducts()
+		{
+			ICollection<Product> products = new HashSet<Product>();
+
+			Product product;
+
+			product = new Product
+			{
+				Id = 1,
+				Name = "White Bread",
+				ImageUrl = "https://media.istockphoto.com/id/995038782/photo/heap-of-bread.jpg?s=612x612&w=0&k=20&c=UoAcNzbbDx2ybqoEZWBaxBdy73W2NN3km8MKSci0cHk="
+
+
+			};
+
+			products.Add(product);
+
+			product = new Product
+			{
+				Id = 2,
+				Name = "Banana Muffin",
+				ImageUrl = "https://hellofrozenbananas.com/wp-content/uploads/2022/08/Gluten-Free-Muffins.jpg"
+			};
+			
+			products.Add(product);
+
+			product = new Product
+			{
+				Id = 3,
+				Name = "Chocolate Cake",
+				ImageUrl = "https://sugarfreelondoner.com/wp-content/uploads/2020/12/sugar-free-birthday-cake-chocolate-1200.jpg"
+
+			};
+
+			products.Add(product);
+
+			return products.ToArray();
+
+		
+		}
+	}
+}
