68ba835f21c5f3676591203604451d1ff6c83e4f
Denis Georgiev <d3nata123@gmail.com>
2023-08-03 23:21:03 +0300

    modifing views and adding controllers

---
+++ BakerySystem.Web/Controllers/HiringController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BakerySystem.Web.Controllers
+{
+	public class HiringController1 : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ BakerySystem.Web/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿namespace BakerySystem.Web.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	public class ProductController : Controller
+	{
+		public async Task<IActionResult> All()
+		{
+			return View();
+		}
+	}
+}
