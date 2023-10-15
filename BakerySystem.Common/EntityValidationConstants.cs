3b68b79a5738e61438fe153ef488263d04a0fcb7
Denis Georgiev <d3nata123@gmail.com>
2023-10-15 17:29:39 +0300

    Add new project to scaffold the indetity for register and login. Implement custom registration

--- BakerySystem.Common/EntityValidationConstants.cs
+++ BakerySystem.Common/EntityValidationConstants.cs
@@ -100,6 +100,8 @@
 
 		public static class User
 		{
+			public const int PasswordMinLength = 6;
+			public const int PasswordMaxLength = 100;
 
 			public const int FirstNameMinLength = 2;
 			public const int FirstNameMaxLength = 15;
---
+++ BakerySystem.Web/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿namespace BakerySystem.Web.Controllers
+{
+	using Microsoft.AspNetCore.Identity;
+	using Microsoft.AspNetCore.Mvc;
+
+	using BakerySystem.Data.Models;
+	using BakerySystem.Web.ViewModels.User;
+
+	public class UserController : Controller
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly IUserStore<ApplicationUser> _userStore;
+
+        public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+			this._signInManager = signInManager;
+			this._userManager = userManager;
+				
+        }
+
+        [HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterFormModel model)
+		{
+
+			if (!ModelState.IsValid)
+			{
+
+				return View(model);
+
+			}
+
+			ApplicationUser user = new ApplicationUser()
+			{
+				FirstName = model.FirstName,
+				LastName = model.LastName,
+
+			};
+
+			await this._userManager.SetEmailAsync(user, model.Email);
+			await this._userManager.SetUserNameAsync(user,string.Join(" ", model.FirstName + model.LastName));
+
+			IdentityResult result = 
+				await this._userManager.CreateAsync(user, model.Password);
+
+			if (!result.Succeeded)
+			{
+				foreach (IdentityError error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+
+				return View(model);
+			}
+				await this._signInManager.SignInAsync(user, false);
+
+				return RedirectToAction("Index", "Home");
+		}
+	}
+}
