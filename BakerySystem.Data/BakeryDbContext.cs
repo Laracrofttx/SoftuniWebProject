14306ef498232528516678547b7dabd3c971c660
Denis Georgiev <d3nata123@gmail.com>
2023-10-30 23:06:11 +0200

    Add UnitTesting and write UnitTests on Product and Category services

--- BakerySystem.Data/BakeryDbContext.cs
+++ BakerySystem.Data/BakeryDbContext.cs
@@ -13,8 +13,7 @@
 		public BakeryDbContext(DbContextOptions<BakeryDbContext> options)
 			: base(options)
 		{
-		
-
+			
 		}
 
 		public DbSet<Category> Categories { get; set; }
---
+++ BakerySystem.Services.Tests/ProductServiceTests.cs
@@ -0,0 +1,69 @@
+namespace BakerySystem.Services.Tests
+{
+	using BakerySystem.Data.Models;
+	using BakerySystem.Services.Interfaces;
+	using BakerySystem.Web.Data;
+	using Microsoft.EntityFrameworkCore;
+
+	using static DataBaseSeeder;
+	public class ProductServiceTests
+	{
+		private DbContextOptions<BakeryDbContext> dbOptions;
+		private BakeryDbContext dbContext;
+
+		private  IProductService productService;
+		private  ICategoryService categoryService;
+
+		public ProductServiceTests()
+		{
+
+
+
+		}
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+
+			this.dbOptions = new DbContextOptionsBuilder<BakeryDbContext>()
+				.UseInMemoryDatabase("BakerySystemInMemory" + Guid.NewGuid().ToString())
+				.Options;
+
+			this.dbContext = new BakeryDbContext(this.dbOptions);
+
+			dbContext.Database.EnsureCreated();
+
+			SeedDataBase(this.dbContext);
+
+			this.productService = new ProductService(this.dbContext);
+			this.categoryService = new CategoryService(this.dbContext);
+
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public async Task ProductExistByIdShouldReturnTrueWhenExist()
+		{
+			int productId = Products.Id;
+
+			bool result = await this.productService.ExistByIdAsynch(productId);
+
+			Assert.True(result);
+		
+		}
+
+		[Test]
+		public async Task CategoryExistByIdShouldReturnTrueWhenExist()
+		{
+			int categoryId = Categories.Id;
+
+			bool result = await this.categoryService.ExistByIdAsync(categoryId);
+
+			Assert.True(result);
+		}
+	}
+}
